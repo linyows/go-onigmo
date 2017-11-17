af450e45d7c0e86b3a2504d03aaa277f8f8321d4
linyows <linyows@gmail.com>
2017-11-17 23:15:13 +0900

    fix ldflags

--- regexp.go
+++ regexp.go
@@ -2,7 +2,7 @@ package onigmo
 
 /*
 #cgo CFLAGS: -I/usr/local/include
-#cgo LDFLAGS: -L/usr/local/lib -lonig
+#cgo LDFLAGS: -L/usr/local/lib -lonigmo
 #include <stdlib.h>
 #include <string.h>
 #include <onigmo.h>
