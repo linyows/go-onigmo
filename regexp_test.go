0e54af5f2022d8edc5e126b616804dbeecab5a30
linyows <linyows@gmail.com>
2017-11-19 00:15:22 +0900

    add test regex

--- regexp_test.go
+++ regexp_test.go
@@ -10,3 +10,38 @@ func TestOnigmoVersion(t *testing.T) {
 		t.Errorf("OnigmoVersion wrong: %s", v)
 	}
 }
+
+func TestRegex(t *testing.T) {
+	regex, err := Compile("^1st user (?<user>[a-z]*) ?2nd user (?<user>[a-z]+) value (?<value>[0-9]+)$")
+	if err != nil {
+		t.Errorf("Compile error: %#v", err)
+	}
+
+	result, err := regex.Match("1st user foo 2nd user bar value 5")
+	if err != nil {
+		t.Errorf("Match error: %#v", err)
+	}
+
+	user, err := result.Get("user")
+	if err != nil {
+		t.Errorf("Get error: %#v", err)
+	}
+
+	if user != "foo" {
+		t.Errorf("User wrong: %s", user)
+	}
+
+	/*
+		value, err := result.Get("value")
+		if err != nil {
+			t.Errorf("Get error: %#v", err)
+		}
+
+		if value != "5" {
+			t.Errorf("Val wrong: %s", value)
+		}
+	*/
+
+	defer regex.Free()
+	defer result.Free()
+}
