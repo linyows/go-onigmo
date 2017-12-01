8040fcc59003a3d0c463e3bbf2770d270c96aa19
linyows <linyows@gmail.com>
2017-12-02 03:14:43 +0900

    add CGO helper as C

--- regexp.go
+++ regexp.go
@@ -10,11 +10,17 @@ package onigmo
 // cgo does not support vargs
 extern int onigmo_helper_error_code_with_info_to_str(UChar* err_buf, int err_code, OnigErrorInfo *errInfo);
 extern int onigmo_helper_error_code_to_str(UChar* err_buf, OnigPosition err_code);
+extern char* onigmo_helper_get(char* str, OnigPosition* beg, OnigPosition* end, int n);
+
 int onigmo_helper_error_code_with_info_to_str(UChar* err_buf, int err_code, OnigErrorInfo *errInfo) {
     return onig_error_code_to_str(err_buf, err_code, errInfo);
 }
 int onigmo_helper_error_code_to_str(UChar* err_buf, OnigPosition err_code) {
     return onig_error_code_to_str(err_buf, err_code);
+}
+char* onigmo_helper_get(char* str, OnigPosition* beg, OnigPosition* end, int n) {
+    char *res = strndup((char*)(str + beg[n]), end[n] - beg[n]);
+    return res;
 }
 */
 import "C"
