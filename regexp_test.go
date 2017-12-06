68d9eaaea02529a3ccd5112652bd96630a70468f
linyows <linyows@gmail.com>
2017-12-06 14:52:01 +0900

    add invalid test

--- regexp_test.go
+++ regexp_test.go
@@ -43,3 +43,38 @@ func TestRegex(t *testing.T) {
 	defer regex.Free()
 	defer result.Free()
 }
+
+func TestInvalidCaptureGroups(t *testing.T) {
+	regex, err := Compile("^1st user (?<user>[a-z]*) ?2nd user (?<user>[a-z]+) (?<x>.*)(.*)value (?<val>[0-9]*)$")
+	if err != nil {
+		t.Error(err)
+	}
+
+	match, err := regex.Match("1st user foo 2nd user bar value 789")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !match.IsMatch() {
+		t.Error("expected a match")
+	}
+	for _, data := range [][]string{
+		[]string{"void", ""},
+		[]string{"", ""},
+	} {
+		_, err := match.Get(data[0])
+		if err == nil {
+			t.Error("Expected error, because used non-existing capture group name.")
+		}
+	}
+	val, err := match.Get("x")
+	if err != nil {
+		t.Error(err)
+	}
+	if val != "" {
+		t.Errorf("Expected empty string, but got %v", val)
+	}
+
+	defer regex.Free()
+	defer match.Free()
+}
